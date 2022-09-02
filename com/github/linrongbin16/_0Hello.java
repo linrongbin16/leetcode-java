1928383822fabc208c1734c30e60e1fa219e1757
linrongbin16 <linrongbin16@outlook.com>
2022-09-02 22:02:54 +0800

    save

---
+++ com/github/linrongbin16/_0Hello.java
@@ -0,0 +1,10 @@
+package com.github.linrongbin16;
+
+public class _0Hello {
+    public static void Main(String[] args) {
+        System.out.println("Hello World");
+        for (int i = 0; i < 10; ++i) {
+            System.out.println("Hello " + i);
+        }
+    }
+}
